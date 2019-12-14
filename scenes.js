c1eb072cf132dab49571e57370fcc4a4b3323979
Kmek <kmekeefe@gmail.com>
2019-12-14 07:06:21 -0500

    Add basic loadscene function

--- scenes.js
+++ scenes.js
@@ -1,5 +1,8 @@
 // All Macbeth scenes are stored in the scenes variable
 
 var scenes = {
-
+    testScene: {
+        description: "asdfg",
+        options: ["asdfg1", "asdfg2", "etc"]
+    },
 }
--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 // Main JS for the Macbeth Choose Your Own Adventure
 
-//******************** asdfg ********************//
+//******************** Constants ********************//
+const storyDiv = document.getElementById("story")
+const buttonDiv = document.getElementById("buttons")
+
+//******************** Load Scene Button ********************//
+function loadScene(scene) {
+    console.log(scene)
+    var newDiv = document.createElement("div")
+    newDiv.setAttribute("class", "row")
+
+    var storyText = document.createElement("p")
+    storyText.innerHTML = scene.description
+    newDiv.append(storyText)
+    
+    storyDiv.append(newDiv)
+}
+
+loadScene(scenes.testScene)
+loadScene(scenes.testScene)
+loadScene(scenes.testScene)
