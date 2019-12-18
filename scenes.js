312e7661c46bf44584a0c1ccb71358caa9f3a1b6
Kmek <kmekeefe@gmail.com>
2019-12-18 10:10:16 -0500

    Working on scene 1.5

--- scenes.js
+++ scenes.js
@@ -1,13 +1,16 @@
 // All Macbeth scenes are stored in the scenes variable
+// Scenes are based off of this macbeth text: http://shakespeare.mit.edu/macbeth/full.html
 
 var scenes = {
-	// Act 1
+    //******************** Act 1 ********************//
+    //******* Scene 1.1 *******//
     sistersIntro: {
         description: ["Thunder and lightning. Enter three witches", "First Witch: When shall we meet again?", "Second Witch: When the battle's lost and won.", "Third Witch: At sunset.", "First Witch: Where?", "Second Witch: Upon the heath.", "Third Witch: There to meet with Macbeth.", "First Witch: Anon.", "All: Fair is foul, and foul is fair; Hover through the fog and filthy air.", "Exeunt"],
         question: "Begin your adventure?",
         options: ["Start"],
         links: ["duncanIntro"]
     },
+    //******* Scene 1.2 *******//
     duncanIntro: {
         description: ["Alarum within. Enter Duncan, Malcolm, Donalbain, Lennox, with Attendants, meeting a bleeding Sergeant", "Duncan: Aah! A bloody soldier!", "Malcolm: Don't worry, he brings news from battle.", "Sergeant: The merciless Macdonwald has been slain by none other than the brave Macbeth. He sliced the villain down his front and mounted his dismembered head on our battlements", "Duncan: Joyous news!", "Sergeant: Indeed, but the Norweyan lord is beginning a fresh assault as we speak", "Duncan: No worries, how are the captains Macbeth and Banquo?", "Sergeant: As much as I could see, they were both in the thick of it. But I can speak no longer, my gashes cry for help."],
         question: "What will Duncan do?",
@@ -27,44 +30,108 @@ var scenes = {
         links: ["thaneCawdor"]
     },
     thaneCawdor: {
-        description: ["Duncan: Hey, who goes there?", "Enter Ross", "Malcolm: The worthy thane of Ross", "Lennox: What is that look in your eyes? What have you seen?", "Ross: God save the king!", "Duncan: Where have you travelled from thane?", "Ross: From Fife, great king. Norway has terrible numbers, but has assistance form the traitorous thane of Cawdor. But that Bellona's bridegroom confronted and crushed his lavish spirits, letting the victory fall on us.", "Duncan: Great news again!", "Ross: But now the Norweyan King craves mercy, presenting ten thousand dollars to our general use.", "Duncan: No more will the thane of Cawdor deceive our interests, pronounce him dead. Greet Macbeth with his former title.", "Ross: I'll see it done.", "Duncan: What he hath lost noble Macbeth hath won."],
+        description: ["Duncan: Hey, who goes there?", "Enter Ross", "Malcolm: The worthy thane of Ross", "Lennox: What is that look in your eyes? What have you seen?", "Ross: God save the king!", "Duncan: Where have you travelled from thane?", "Ross: From Fife, great king. Norway has terrible numbers, but has assistance form the traitorous thane of Cawdor. But that Bellona's bridegroom confronted and crushed his lavish spirits, letting the victory fall on us.", "Duncan: Great news again!", "Ross: But now the Norweyan King craves mercy, presenting ten thousand dollars to our general use.", "Duncan: No more will the thane of Cawdor deceive our interests, pronounce him dead. Greet Macbeth with his former title.", "Ross: I'll see it done.", "Duncan: What he hath lost noble Macbeth hath won.", "Exeunt"],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["chestnutStory"]
+    },
+    //******* Scene 1.3 *******//
+    chestnutStory: {
+        description: ["Thunder. Enter the three Witches", "First Witch: Where have you been sister?", "Second Witch: Killing swine.", "Third Witch: And you, sister?", "First Witch: A sailor's wife had chestnuts in her lap, and munched and munched and munched. 'Give me' I asked. 'Aroint thee, witch!' the rump-fed ronyon cried. Her husband's gone to Aleppo, master o'the Tiger. But in a sieve I'll thither sail. Like a rat without a tail, I'll do, I'll do, and I'll do.", "Second Witch: I'll give thee a wind.", "First Witch: You're kind. Look what I have.", "Second Witch: Show me, show me.", "First Witch: Here I have a pilot's thumb. Wrecked as homeward he did come.", "Drum within", "Third Witch: A drum, a drum!, Macbeth is here!"],
         question: "Continue",
         options: ["Continue"],
         links: ["macbethBanquoEncounterWitches"]
     },
-    /* chestnutStory: {
-        description: ["First Witch: Where have you been sister?", "Second Witch: Killing swine.", "Third Witch: And you, sister?", "First Witch: A sailor's wife had chestnuts in her lap, and munched and munched and munched. 'Give me' I asked. 'Aroint thee, witch!' the rump-fed ronyon cried. Her husband's gone to Aleppo, master o'the Tiger. But in a sieve I'll thither sail. Like a rat without a tail, I'll do, I'll do, and I'll do.", "Second Witch: I'll give thee a wind.", "First Witch: You're kind. Look what I have.", "Second Witch: Show me, show me.", "First Witch: Here I have a pilot's thumb. Wrecked as homeward he did come.", "Third Witch: A drum, a drum!, Macbeth is here!"],
-    }, */
     macbethBanquoEncounterWitches: {
-    	description: ["Macbeth: so foul and fair a day I have not seen.", "Banquo: How far is it to Forres? What are these creatures in withered and wild attire? They don't look like inhabitants of the earth, and yet they are so."],
+    	description: ["Enter Macbeth and Banquo", "Macbeth: So foul and fair a day I have not seen.", "Banquo: How far is it to Forres? What are these creatures in withered and wild attire? They don't look like inhabitants of the earth, and yet they are so."],
         question: "What will Macbeth do?",
         options: ["Speak to the creatures", "Attack the creatures"],
         links: ["speakToCreatures", "attackCreatures"]
     }, 
     attackCreatures: {
-        description: ["Macbeth: Whatever you are, you shouldn't be here! Begone, or prepare to perish!", "Banquo: At arms!", "First Witch: You may try, feeble ones", "Second Witch: But be warned, no blade shall pierce our flesh.", "Third Witch: You shouldn't have resorted to such violence, Macbeth."],
-        question: "todo",
-        options: ["todo"],
-        links: ["todo"]
+        description: ["Macbeth: Whatever you are, you shouldn't be here! Begone, or prepare to perish!", "Banquo: At arms!", "First Witch: You may try, feeble ones", "Second Witch: But be warned, no blade shall pierce our flesh.", "Third Witch: You shouldn't have resorted to such hostility, Macbeth, king hereafter.", "Macbeth: King? I shall become king?", "First Witch: Indeed."],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["banquoProphecy"]
     },
     speakToCreatures: {
-        description: ["Macbeth: Speak, if you can: what are you?"],
+        description: ["Macbeth: Speak, if you can, what are you?", "First Witch: All hail Macbeth! Hail to thee, thane of Glamis!", "Second Witch: All hail Macbeth, hail to thee, thane of Cawdor", "Third Witch: All hail Macbeth, thou shalt be king hereafter!"],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["banquoProphecy"]
+    },
+    banquoProphecy: {
+        description: ["Banquo: Good sirs, you greet with present grace and great prediction of noble having and royal hope, but to me you speak not. If you can look into the seeds of time and say which grain will grow and which will not, speak then to me! I neither beg nor fear your favors nor your hate.", "First Witch: Hail!", "Second Witch: Hail!", "Third Witch: Hail!", "First Witch: Lesser than Macbeth, and greater.", "Second Witch: Not so happy, yet much happier.", "Third Witch: Thou shalt get kings, though thou be none. So all hail, Macbeth and Banquo!", "First Witch: Banquo and Macbeth, all hail!"],
+        question: "What will Macbeth do?",
+        options: ["Demand more info", "Run away"],
+        links: ["macbethDemandsMore", "macbethRunsAway"]
+    },
+    macbethRunsAway:  {
+        description: ["Macbeth: Strange creatures from the beyond, I cannot believe your false words. This is too much!", "Macbeth runs away", "Banquo: I understand your spoken words, but I too cannot fall for your tricks.", "Banquo runs away to follow Macbeth"],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["macbethBanquoReasonWitches"]
+    },
+    macbethDemandsMore: {
+        description: ["Macbeth: Stay, you imperfect speakers, tell me more! I know I am thane of Glamis, but how of Cawdor? That thane lives. To be king stands not within the prospect of belief, no more than to be the thane of Cawdor. How do you know this strange intelligence? Why upon this blasted heath you stop our way with such prophetic greeting? Speak, I charge you!", "Witches vanish", "Banquo: The earth hath bubbles, as the water has, and these are of them. Wither are they vanished?", "Macbeth: Into the air; and what seemed corporal melted as breath into the wind. Would they have stayed!"],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["macbethBanquoReasonWitches"]
+    },
+    macbethBanquoReasonWitches: {
+        description: ["Banquo: Were such things here as we do speak about? Or have we eaten on the insane root that takes the reason prisoner?", "Macbeth: Your children shall be kings.", "Banquo: You shall be king.", "Macbeth: And thane of Cawdor too. Went it not so?", "Banquo: To the selfsame tune and words. Who's here?", "Enter Ross and Angus"],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["macbethThaneCawdor"]
+    },
+    macbethThaneCawdor: {
+        description: ["Ross: The king hath happily received, Macbeth, the news of thy success, and when he reads thy personal venture in the rebels' fight, his wonders and his praises do contend which should be thine or his. Silenced with that, in viewing o'er the rest o' the selfsame day, he finds thee in the stout Norweyan ranks, nothing afeard of what thyself didst make, strange images of death. As thick as hail came post with post, and every one did bear thy praises in his kingdom's great defence, and pour'd them down before him.", "Angus: We are sent to give thee from our royal master thanks. Only to herald thee into his sight, not pay thee.", "Ross: And, for an earnest of a greater honour, he bade me, from him, call thee thane of Cawdor. In which addition, hail, most worthy thane! For it is thine.", "Banquo: What, can the devil speak true?"],
+        question: "What will Macbeth do?",
+        options: ["Disbelieve Ross", "Accept the position"],
+        links: ["macbethThaneCawdorDenied", "macbethThaneCawdorAccepted"]
+    },
+    macbethThaneCawdorDenied: {
+        description: ["Macbeth: The thane of Cawdor lives, why do you dress me in borrowed robes?", "Angus: Who was the thane lives yet. But under heavy judgment bears that life which he deserves to lose. Whether he was combined with those of Norway, or did line the rebel with hidden help and vantage, or that with both he labour'd in his country's wreck, I know not. But treasons capital, confess'd and proved, have overthrown him.", "Macbeth: How did this happen so suddenly? I must know the truth!", "Ross: Calm down Worthy Macbeth, you have it.", "Macbeth [aside]: Could the creatures be true? No, impossible. I cannot fall for their trickery. I must find out the truth from one who could never be tricked.", "Macbeth: We must go to the king, I need to hear this from him.", "Ross: Very well."],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["macbethMustSeeKing"]
+    },
+    macbethThaneCawdorAccepted: {
+        description: ["Macbeth: The thane of Cawdor, I am honored.", "Angus: Dear Macbeth, you do not seem surprised?", "Macbeth: Let's just say it was to be expected. Everyone could see the former thane was growing untrustworthy, so it was only a matter of time.", "Macbeth [Aside]: Glamis, and thane of Cawdor! The greatest is behind.", "To Ross and Angus", "Macbeth [aside]: Thank you for your pains.", "To Banquo", "Macbeth [aside]: Do you not hope your children shall be kings, when those that gave the thane of Cawdor to me promised no less to them?", "Banquo: That trusted home might yet enkindle you unto the crown, besides the thane of Cawdor. But 'tis strange and oftentimes, to win us to our harm, the instruments of darkness tell us truths, win us with honest trifles, to betray's in deepest consequence.", "Macbeth: Come, I must see the king. I must thank him for this gift.", "Banquo: Very gladly.", "Macbeth: Till then, enough. Come, friends.", "Exeunt"],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["macbethMustSeeKing"]
+    },
+    //******* Scene 1.4 *******//
+    macbethMustSeeKing: {
+        description: ["Forres, the palace. Flourish. Enter Duncan, Malcolm, Donalbain, Lennox, and Attendants", "Duncan: Is execution done on Cawdor? Are not those in commission yet return'd?", "Malcolm: My liege, they are not yet back. But I have spoke with one that saw him die, who did report that very frankly he confess'd his treasons.", "Duncan: There's no art to find the mind's construction in the face. He was a gentleman on whom I built an absolute trust.", "Enter Macbeth, Banquo, Ross, and Angus", "Duncan: O worthiest cousin! My worthy Cawdor! The sin of my ingratitude even now was heavy on me. Only I have left to say, more is thy due than more than all can pay.", "Macbeth [aside]: The king has said so, I am the true thane of Cawdor.", "Macbeth: The service and the loyalty I owe, in doing it, pays itself.", "Duncan: Welcome hither. Noble Banquo, that hast no less deserved, nor must be known no less to have done so. Let me enfold thee and hold thee to my heart.", "Banquo: There if I grow, the harvest is your own.", "King Duncan and Banquo hug"],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["princeMalcolm"]
+    }, 
+    princeMalcolm: {
+        description: ["Duncan: My plenteous joys, we will establish our estate upon our eldest, Malcolm, whom we named hereafter the Prince of Cumberland, which honor must not unaccompanied invest him only. But signs like nobleness, like stars, shall shine on all deservers. From hence to Inverness, and bind us further to you.", "King Duncan's eldest son, Malcolm, is crowned prince", "Macbeth: I'll be myself the harbinger and make joyful the hearing of my wife with your approach, so humbly take my leave.", "Macbeth [aside]: The Prince of Cumberland! that is a step on which I must fall down, or else o'erleap, for in my way it lies. Stars, hide your fires; let not light see my black and deep desires. The eye wink at the hand; yet let that be, which the eye fears, when it is done, to see.", "Exit Macbeth", "Duncan: He is full so valiant, and in his commendations I am fed. It is a banquet to me. Let's after him, whose care is gone before to bid us welcome.", "Flourish. Exeunt"],
+        question: "Continue",
+        options: ["Continue"],
+        links: ["ladyMacbethLetter"]
+    },
+    //******* Scene 1.5 *******//
+    ladyMacbethLetter: {
+        description: ["Enter Lady Macbeth, reading a letter", "Lady macbeth: 'They met me in the day of success, and I have learned by the perfectest report, they have more in them than mortal knowledge. When I burned in desire to question them further, they made themselves air, into which they vanished. Whiles I stood rapt in the wonder of it, came missives from the king, who all-hailed me 'Thane of Cawdor;' by which title, before, these weird sisters saluted me, and referred me to the coming on of time, with 'Hail, king that shalt be!' This have I thought good to deliver thee, my dearest partner of greatness, that thou mightst not lose the dues of rejoicing, by being ignorant of what greatness is promised thee. Lay it to thy heart, and farewell.'", "Lady Macbeth: Glamis thou art, and Cawdor; and shalt be what thou art promised. yet do I fear thy nature; it is too full o' the milk of human kindness to catch the nearest way. Thou wouldst be great; art not without ambition, but without the illness should attend it. What thou wouldst highly, that wouldst thou holily; wouldst not play false, and yet wouldst wrongly win. thou'ldst have, great Glamis, that which cries 'Thus thou must do, if thou have it; and that which rather thou dost fear to do than wishest should be undone.' Hie thee hither, that I may pour my spirits in thine ear; and chastise with the valour of my tongue all that impedes thee from the golden round, which fate and metaphysical aid doth seem to have thee crown'd withal.", "Enter a messenger", "Lady Macbeth: What is your tidings?"],
         question: "todo",
         options: ["todo"],
         links: ["todo"]
     },
- 
+
+
+    // 0-1-2-4-5-6-8-9-11-12-13-15-16-17-18
+
+    //******************** Todo Scene ********************//
     todo: {
         description: ["todo: todo"],
         question: "todo",
         options: ["todo"],
         links: ["todo"]
     },
-    testScene: {
-        description: ["testScene text"],
-        question: "what do you choose?",
-        options: ["testScene", "todo"],
-        links: ["testScene", "todo"]
-    }
 }
 
